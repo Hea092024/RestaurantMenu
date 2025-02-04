31a97c95588dabd6e9c50c00e217380f7112b68e
Hector Acevedo <HEA092024@gmail.com>
2025-02-04 14:35:36 +0100

    .

---
+++ src/Dish.jsx
@@ -0,0 +1,15 @@
+
+const myCard = (props) =>{
+    return (
+        <div id="card_holder" >
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+
+        </div>
+    )
+}
+
+export default myCard
