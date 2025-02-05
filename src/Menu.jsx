fc6fe016b797de2f66e2579c26247508c3967f06
Hector Acevedo <HEA092024@gmail.com>
2025-02-05 17:01:04 +0100

    .

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,7 +3,7 @@ import Dish from "./Dish";
 function Menu({ menu }) {
   return (
     <>
-      <h1 id="title">Meny</h1>
+      <h1 id="title">Restaurant Menu</h1>
       <ul id="menu">
         {menu.map((dish) => (
           <Dish key={dish.id} dish={dish} />
