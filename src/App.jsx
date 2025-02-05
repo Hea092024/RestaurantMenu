8a403a2e69b5852e86264cf34f0013a9b728fa9e
Hector Acevedo <HEA092024@gmail.com>
2025-02-05 17:01:52 +0100

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import meny from "./Meny";
-import MyMenu from "./Dish";  // Update import
+import Menu from "./Menu.jsx";
+import MenuData from "./menuData.js";
 
 function App() {
-  return (
-    <div className="container mx-auto p-6">
-      {/* Header */}
-      <header className="text-center mb-10">
-        <h1 className="text-5xl font-extrabold text-gray-800">
-          Restaurant Menu
-        </h1>
-        <p className="text-xl text-gray-600">Our finest dishes just for you</p>
-      </header>
-
-      {/* Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {meny.map((obj) => (
-          <MyMenu key={obj.id} {...obj} />  // Pass the object as props
-        ))}
-      </div>
-    </div>
-  );
+  return <Menu menu={MenuData} />;
 }
 
 export default App;
