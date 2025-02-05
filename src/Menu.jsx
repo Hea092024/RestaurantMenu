5c832bf9b1dbf499da9164e2cef53de231f4ee12
Hector Acevedo <HEA092024@gmail.com>
2025-02-05 18:02:28 +0100

    small style change

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,8 +2,8 @@ import Dish from "./Dish";
 
 function Menu({ menu }) {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-extrabold text-white mb-8">
+    <div className="text-center px-4">
+      <h1 className="text-5xl font-extrabold text-white mb-10 leading-tight">
         Restaurant Menu
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
